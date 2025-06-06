7999219ef5fb2eead96ef2fffe419cc7169daec0
Marcin Magdziak <marcinm@live.nl>
2025-06-06 14:31:56 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log('Hier komt je server voor Sprint 12.')
+// general
+import express from "express";
+import { Liquid } from "liquidjs";
+import "dotenv/config";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+
+app.set("views", "./views");
+
+// Index route
+app.get("/", async function (request, response) {
+
+  // Geef hier aan welke pagina getoond moet worden op de / (home) route, en wat er naar de pagina gestuurd wordt
+  response.render("index.liquid", {});
+});
+
+app.set("port", process.env.PORT || 1234);
+
+app.listen(app.get("port"), function () {
+  console.log(`Project draait via http://localhost:${app.get("port")}/`);
+});
