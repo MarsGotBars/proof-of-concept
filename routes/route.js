e9a9a2853a7d60e7cd2b2f016da2b65a1e44c1d9
MarsGotBars <marcinm@live.nl>
2025-06-18 02:50:13 +0200

    post

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 import indexRoute from './getIndex.js';
 import overviewRoute from './getOverview.js';
 import detailsRoute from './getDetails.js';
+import postMessageRoute from './postMessage.js';
 
 export default function(app) {
   app.use('/', indexRoute);
   app.use('/', detailsRoute);
+  app.use('/', postMessageRoute);
   app.use('/', overviewRoute);
 }
